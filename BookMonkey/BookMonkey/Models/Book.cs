b1eb6a0ed49e451c180b32cf62353a5eb7a12932
Sebastian Kleinschmager <himself@schmaga.de>
2017-04-23 23:15:25 +0200

    Create a static book list

---
+++ BookMonkey/BookMonkey/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMonkey.Models
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Abstract { get; set; }
+        public int NumPages { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+    }
+}
