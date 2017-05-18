df2e6a8f50ed2fda17d3a14cc51be064f426ed76
Sebastian Kleinschmager <himself@schmaga.de>
2017-05-18 13:35:06 +0200

    Implement Detail & Update REST-API

--- BookMonkey/BookMonkey/Controllers/BookApiController.cs
+++ BookMonkey/BookMonkey/Controllers/BookApiController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using BookMonkey.Services;
+using BookMonkey.Services.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookMonkey.Controllers
@@ -20,6 +21,22 @@ namespace BookMonkey.Controllers
         {
             var books = await _bookService.GetAllBooks();
             return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("{isbn}")]
+        public async Task<IActionResult> GetBook(string isbn)
+        {
+            var book = await _bookService.GetByIsbn(isbn);
+            return Ok(book);
+        }
+
+        [HttpPut]
+        [Route("{isbn}")]
+        public async Task<IActionResult> UpdateBook(string isbn, [FromBody] Book book)
+        {
+            await _bookService.UpdateBook(book);
+            return Ok();
         }
     }
 }
